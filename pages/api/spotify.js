b79f4d67de797f34f05ddf4446cf7a7755870abe
Zeyu Yao <novodoodle@gmail.com>
2023-02-03 21:51:11 +0800

    Update artists formatting

--- pages/api/spotify.js
+++ pages/api/spotify.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
     const song = await response.json();
     const isPlaying = song.is_playing;
     const title = song.item.name;
-    const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
+    const artist = song.item.artists.map((artist) => artist.name).join(', ').replace(/,(?!.*,)/gmi, ' and');
     const album = song.item.album.name;
     const albumImageUrl = song.item.album.images[0].url;
     const songUrl = song.item.external_urls.spotify;
