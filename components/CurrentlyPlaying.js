881d68636ab87d73af83086f05c2b0df8853a0a3
Zeyu Yao <novodoodle@gmail.com>
2023-04-19 14:03:46 +0800

    Add 3D tilt effect

--- components/CurrentlyPlaying.js
+++ components/CurrentlyPlaying.js
@@ -7,6 +7,7 @@ import styles from "@/styles/CurrentlyPlaying.module.css";
 
 // Assets
 import SpotifyLogo from "@/assets/spotify.svg";
+import Tilt from "./Tilt";
 
 export default function Spotify() {
   const fetcher = (url) => fetch(url).then((r) => r.json());
@@ -32,29 +33,31 @@ export default function Spotify() {
   }, []);
 
   return (
-    <div className={styles.container}>
-      {isLoaded ? (
-        <div className={styles.header}>
-          <Image
-            src={SpotifyLogo}
-            alt="Spotify Logo"
-            width={24}
-            height={24}
-            className={styles.logo}
-          />
-          {song.isPlaying ? (
-            <div>
-              <h2 className={styles.title}>I am currently listening to <span className={styles.underline_on_hover}><Link href={song.songUrl} target="_blank" rel="noopener noreferrer">{song.title}</Link></span> by {song.artist}!</h2>
-            </div>
-          ) : (
-            <h2 className={styles.title}>I am not currently listening to anything.</h2>
-          )}
-        </div>
-      ) : (
-        <div className={styles.header}>
-          <h2 className={styles.title}>Loading...</h2>
-        </div>
-      )}
-    </div>
+    <Tilt>
+      <div className={styles.container}>
+        {isLoaded ? (
+          <div className={styles.header}>
+            <Image
+              src={SpotifyLogo}
+              alt="Spotify Logo"
+              width={24}
+              height={24}
+              className={styles.logo}
+            />
+            {song.isPlaying ? (
+              <div>
+                <h2 className={styles.title}>I am currently listening to <span className={styles.underline_on_hover}><Link href={song.songUrl} target="_blank" rel="noopener noreferrer">{song.title}</Link></span> by {song.artist}!</h2>
+              </div>
+            ) : (
+              <h2 className={styles.title}>I am not currently listening to anything.</h2>
+            )}
+          </div>
+        ) : (
+          <div className={styles.header}>
+            <h2 className={styles.title}>Loading...</h2>
+          </div>
+        )}
+      </div>
+    </Tilt>
   );
 }
---
+++ components/Tilt.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef } from 'react'
+import VanillaTilt from 'vanilla-tilt'
+
+// From Hack Club! https://github.com/hackclub/site/blob/ed39c21a308bbf457c6795629a679e6049bada4c/components/tilt.js
+const Tilt = ({ options = {}, children, ...props }) => {
+  const root = useRef(null)
+
+  useEffect(() => {
+    VanillaTilt.init(root.current, {
+      max: 7.5,
+      scale: 1.05,
+      speed: 400,
+      glare: true,
+      'max-glare': 0.25,
+      gyroscope: false,
+      ...options
+    })
+  }, [options])
+
+  return React.cloneElement(children, { ref: root })
+}
+
+export default Tilt
