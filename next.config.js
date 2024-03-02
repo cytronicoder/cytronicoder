d52832b54aa0c674f065a13e6d8212ce7f9a12d7
Zeyu Yao <novodoodle@gmail.com>
2024-03-02 21:00:27 +0800

    Update image domains to remote patterns

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["i.scdn.co", "slack.cytronicoder.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "slack.cytronicoder.com",
+      },
+    ],
   },
 };
 
