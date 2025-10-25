8760b1f00eb4ae284b6506062849fd2df4369e04
Zeyu Yao <novodoodle@gmail.com>
2025-10-25 15:20:18 +0800

    fix: add missing qualities property to images configuration

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,7 @@ const nextConfig = {
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         minimumCacheTTL: 60 * 60 * 24 * 365,
+        qualities: [75, 85, 90],
     },
     experimental: {
         optimizePackageImports: ['date-fns', 'lodash'],
